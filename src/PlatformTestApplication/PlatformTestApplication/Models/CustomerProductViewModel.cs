b686bfe77595c382bc50fb79de83afed532e1770
Chris Dusyk <chris.dusyk@gmail.com>
2016-02-08 13:57:09 -0600

    Started creating the ViewModels for display

---
+++ src/PlatformTestApplication/PlatformTestApplication/Models/CustomerProductViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlatformTestApplication.Models
+{
+	public class CustomerProductViewModel
+	{
+		public int CustomerProductID { get; set; }
+		public int? ProductID { get; set; }
+		public int? SoldByID { get; set; }
+		public int? CustomerID { get; set; }
+		public DateTime? SoldDate { get; set; }
+
+		public Product Product { get; set; }
+		public Employee Employee { get; set; }
+		public Customer Customer { get; set; }
+	}
+}
---
+++ src/PlatformTestApplication/PlatformTestApplication/Models/CustomerViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlatformTestApplication.Models
+{
+	public class CustomerViewModel
+	{
+		public int CustomerID { get; set; }
+		public string CustomerName { get; set; }
+		public string Address { get; set; }
+		public string City { get; set; }
+		public string Province { get; set; }
+		public string PostalCode { get; set; }
+
+		public IEnumerable<CustomerProductViewModel> CustomerProducts { get; set; }
+	}
+}
