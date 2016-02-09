2de2ecd5d7a81c3c81716ae0755e252c07579406
Chris Dusyk <chris.dusyk@gmail.com>
2016-02-09 13:37:38 -0600

    Added limit to Customer list

--- src/PlatformTestApplication/PlatformTestApplication/Controllers/CustomersController.cs
+++ src/PlatformTestApplication/PlatformTestApplication/Controllers/CustomersController.cs
@@ -10,118 +10,119 @@ using PlatformTestApplication.Models;
 
 namespace PlatformTestApplication.Controllers
 {
-    public class CustomersController : Controller
-    {
-        private CS401_DBEntities db = new CS401_DBEntities();
+	public class CustomersController : Controller
+	{
+		private CS401_DBEntities db = new CS401_DBEntities();
 
-        // GET: Customers
-        public ActionResult Index()
-        {
-            return View(db.Customers.ToList());
-        }
+		// GET: Customers
+		public ActionResult Index()
+		{
+			var smallList = db.Customers.Take(50).ToList();
+			return View(smallList);
+		}
 
-        // GET: Customers/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Customer customer = db.Customers.Find(id);
-            if (customer == null)
-            {
-                return HttpNotFound();
-            }
-            return View(customer);
-        }
+		// GET: Customers/Details/5
+		public ActionResult Details(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Customer customer = db.Customers.Find(id);
+			if (customer == null)
+			{
+				return HttpNotFound();
+			}
+			return View(customer);
+		}
 
-        // GET: Customers/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+		// GET: Customers/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
 
-        // POST: Customers/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "CustomerID,CustomerName,Address,City,Province,PostalCode")] Customer customer)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Customers.Add(customer);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+		// POST: Customers/Create
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create([Bind(Include = "CustomerID,CustomerName,Address,City,Province,PostalCode")] Customer customer)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Customers.Add(customer);
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
 
-            return View(customer);
-        }
+			return View(customer);
+		}
 
-        // GET: Customers/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Customer customer = db.Customers.Find(id);
-            if (customer == null)
-            {
-                return HttpNotFound();
-            }
-            return View(customer);
-        }
+		// GET: Customers/Edit/5
+		public ActionResult Edit(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Customer customer = db.Customers.Find(id);
+			if (customer == null)
+			{
+				return HttpNotFound();
+			}
+			return View(customer);
+		}
 
-        // POST: Customers/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "CustomerID,CustomerName,Address,City,Province,PostalCode")] Customer customer)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(customer).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(customer);
-        }
+		// POST: Customers/Edit/5
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit([Bind(Include = "CustomerID,CustomerName,Address,City,Province,PostalCode")] Customer customer)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Entry(customer).State = EntityState.Modified;
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(customer);
+		}
 
-        // GET: Customers/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Customer customer = db.Customers.Find(id);
-            if (customer == null)
-            {
-                return HttpNotFound();
-            }
-            return View(customer);
-        }
+		// GET: Customers/Delete/5
+		public ActionResult Delete(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Customer customer = db.Customers.Find(id);
+			if (customer == null)
+			{
+				return HttpNotFound();
+			}
+			return View(customer);
+		}
 
-        // POST: Customers/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Customer customer = db.Customers.Find(id);
-            db.Customers.Remove(customer);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+		// POST: Customers/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public ActionResult DeleteConfirmed(int id)
+		{
+			Customer customer = db.Customers.Find(id);
+			db.Customers.Remove(customer);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-    }
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
 }
