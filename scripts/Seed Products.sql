d983df6c56acbf37e6a389c1d2c2c0011308e72d
Chris Dusyk <chris.dusyk@gmail.com>
2016-02-08 23:24:51 -0600

    Completed Platform components for presentation
    Products and CustomerProducts controllers completed, and all views for them scaffolded.

---
+++ scripts/Seed Products.sql
@@ -0,0 +1,26 @@
+INSERT INTO Products(ProductCode, ProductName)
+VALUES ('TEST1', 'Test Product 1');
+
+INSERT INTO Products(ProductCode, ProductName)
+VALUES ('TEST2', 'Test Product 2');
+
+INSERT INTO Products(ProductCode, ProductName)
+VALUES ('TEST3', 'Test Product 3');
+
+INSERT INTO Products(ProductCode, ProductName)
+VALUES ('TEST4', 'Test Product 4');
+
+INSERT INTO Products(ProductCode, ProductName)
+VALUES ('TEST5', 'Test Product 5');
+
+INSERT INTO Products(ProductCode, ProductName)
+VALUES ('TEST6', 'Test Product 6');
+
+INSERT INTO Products(ProductCode, ProductName)
+VALUES ('TEST7', 'Test Product 7');
+
+INSERT INTO Products(ProductCode, ProductName)
+VALUES ('TEST8', 'Test Product 8');
+
+INSERT INTO Products(ProductCode, ProductName)
+VALUES ('TEST9', 'Test Product 9');
---
+++ src/PlatformTestApplication/PlatformTestApplication/Controllers/CustomerProductsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PlatformTestApplication.Models;
+
+namespace PlatformTestApplication.Controllers
+{
+    public class CustomerProductsController : Controller
+    {
+        private CS401_DBEntities db = new CS401_DBEntities();
+
+        // GET: CustomerProducts
+        public ActionResult Index()
+        {
+            var customerProducts = db.CustomerProducts.Include(c => c.Product).Include(c => c.Employee).Include(c => c.Customer);
+            return View(customerProducts.ToList());
+        }
+
+        // GET: CustomerProducts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CustomerProduct customerProduct = db.CustomerProducts.Find(id);
+            if (customerProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customerProduct);
+        }
+
+        // GET: CustomerProducts/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProductID = new SelectList(db.Products, "ProductID", "ProductCode");
+            ViewBag.SoldByID = new SelectList(db.Employees, "EmployeeID", "FirstName");
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "CustomerName");
+            return View();
+        }
+
+        // POST: CustomerProducts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CustomerProductID,ProductID,SoldByID,CustomerID,SoldDate")] CustomerProduct customerProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CustomerProducts.Add(customerProduct);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProductID = new SelectList(db.Products, "ProductID", "ProductCode", customerProduct.ProductID);
+            ViewBag.SoldByID = new SelectList(db.Employees, "EmployeeID", "FirstName", customerProduct.SoldByID);
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "CustomerName", customerProduct.CustomerID);
+            return View(customerProduct);
+        }
+
+        // GET: CustomerProducts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CustomerProduct customerProduct = db.CustomerProducts.Find(id);
+            if (customerProduct == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProductID = new SelectList(db.Products, "ProductID", "ProductCode", customerProduct.ProductID);
+            ViewBag.SoldByID = new SelectList(db.Employees, "EmployeeID", "FirstName", customerProduct.SoldByID);
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "CustomerName", customerProduct.CustomerID);
+            return View(customerProduct);
+        }
+
+        // POST: CustomerProducts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CustomerProductID,ProductID,SoldByID,CustomerID,SoldDate")] CustomerProduct customerProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customerProduct).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProductID = new SelectList(db.Products, "ProductID", "ProductCode", customerProduct.ProductID);
+            ViewBag.SoldByID = new SelectList(db.Employees, "EmployeeID", "FirstName", customerProduct.SoldByID);
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "CustomerName", customerProduct.CustomerID);
+            return View(customerProduct);
+        }
+
+        // GET: CustomerProducts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CustomerProduct customerProduct = db.CustomerProducts.Find(id);
+            if (customerProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customerProduct);
+        }
+
+        // POST: CustomerProducts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CustomerProduct customerProduct = db.CustomerProducts.Find(id);
+            db.CustomerProducts.Remove(customerProduct);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
