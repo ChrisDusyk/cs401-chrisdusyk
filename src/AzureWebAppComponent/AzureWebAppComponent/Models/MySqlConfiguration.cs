c0d44a2b232e6371e64868a114be78a18da073aa
Chris Dusyk <chris.dusyk@gmail.com>
2016-03-28 09:27:16 -0600

    Updated packages

---
+++ src/AzureWebAppComponent/AzureWebAppComponent/Models/MySqlConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace AzureWebAppComponent.Models
+{
+	public class MySqlConfiguration : DbConfiguration
+	{
+		public MySqlConfiguration()
+		{
+			SetHistoryContext("MySql.Data.MySqlClient", (conn, schema) => new MySqlHistoryContext(conn, schema));
+		}
+	}
+}
