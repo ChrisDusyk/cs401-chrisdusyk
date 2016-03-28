fa5a0371b90e5ec29c5f2e6a9706f1eac85e770b
Chris Dusyk <chris.dusyk@gmail.com>
2016-03-28 09:52:33 -0600

    Added MySQL connections and provider, and set up identity.

--- src/AzureWebAppComponent/AzureWebAppComponent/Models/IdentityModels.cs
+++ src/AzureWebAppComponent/AzureWebAppComponent/Models/IdentityModels.cs
@@ -6,28 +6,33 @@ using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace AzureWebAppComponent.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
-    {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
-        {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
-            return userIdentity;
-        }
-    }
+	// You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+	public class ApplicationUser : IdentityUser
+	{
+		public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+		{
+			// Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+			var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+			// Add custom user claims here
+			return userIdentity;
+		}
+	}
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+	{
+		static ApplicationDbContext()
+		{
+			Database.SetInitializer(new MySqlInitializer());
+		}
 
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
+		public ApplicationDbContext() : base("AuroraContext")
+		{
+
+		}
+
+		public static ApplicationDbContext Create()
+		{
+			return new ApplicationDbContext();
+		}
+	}
 }
