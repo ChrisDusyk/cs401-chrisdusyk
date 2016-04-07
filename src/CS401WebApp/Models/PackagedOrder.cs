973b6dc1e86c72e8b896045586e9bc398765dea1
Chris Dusyk <chris.dusyk@gmail.com>
2016-04-06 23:56:37 -0600

    Created Azure worker role for the Service Bus
    The Service Bus will transport messages to the Azure worker role, which will handle pushing data into the database.

---
+++ src/CS401WebApp/Models/PackagedOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CS401WebApp.Models
+{
+	public class PackagedOrder
+	{
+		public Order Order { get; set; }
+
+		public List<OrderProduct> OrderProducts { get; set; }
+	}
+}
